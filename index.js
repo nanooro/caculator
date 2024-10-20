c784d2bf1d0a0fcaf77c735a41b957aea742c66e
nanooru <notnotsky26@gmail.com>
2024-10-20 15:08:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let add = 0;
+console.log(add);
+// let nresult = document.querySelector(".result");
+// nresult.textContent = add;
+// const myVariable = add;
+// document.getElementById("myParagraph").textContent = myVariable;
+function update(){
+    let result = document.getElementById("result");
+    result.textContent = add;
+    console.log("updated")
+}
---
+++ test.js
@@ -0,0 +1,4 @@
+let add = 0;
+console.log(add);
+let nresult = document.querySelector(".result");
+nresult.textContent = add;
